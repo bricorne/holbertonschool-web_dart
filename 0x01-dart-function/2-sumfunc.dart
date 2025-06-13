a0789a40b6455bcd6cb6177a99eb537c22c0d206
bricorne <bricepoitevineau@gmail.com>
2025-06-13 10:40:43 +0200

    task 2

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,13 @@
+// 2-sumfunc.dart
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  return "Add $a + $b = ${add(a, b)}\nSub $a - $b = ${sub(a, b)}";
+}
